4e4f8f9661bd975d29b6179fe1d4c45333ade19a
Md Zidan Khan <80000340+zidan420@users.noreply.github.com>
2024-09-05 22:30:01 +0600

    Update networkDiscover.py

--- networkDiscover.py
+++ networkDiscover.py
@@ -1,4 +1,7 @@
+# Our custom library - we are importing the scan function only.
 from mitm_helper_func import scan
+
+# a builtin library to get user input through command line arguments
 import argparse
 
 def print_result(scan_result_list):
@@ -8,12 +11,20 @@ def print_result(scan_result_list):
 		print(client['IP'] + '\t' + client['MAC'])
 
 def main():
+	# Initialize the argparse
 	parser = argparse.ArgumentParser()
+	
+	# Add argument(s) to args
 	parser.add_argument('-t', '--target', help="Target IP")
 	args = parser.parse_args()
-
+	
+	# Store the target argument to target variable
 	target = args.target
+
+	# Scan the target with our custom function
 	scan_result_list = scan(target)
+
+	# Prints the result of our scan
 	print_result(scan_result_list)
 
 if __name__ == '__main__':
